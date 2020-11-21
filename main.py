464910d892b92f94269f8f6a2bd319478c2dee64
GruescuDanziel <gruescu.ipatie@gmail.com>
2020-11-22 01:04:38 +0200

    The main loop for website creating

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Daniel")
+from WebsiteClass import Website
+
+initialAction = int(input("What do you wish to do: 1. Make a new webSite. 2. Edit an old save"))
+
+if initialAction == 1:
+    Website.create()
+
+
