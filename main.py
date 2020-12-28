78f354cb493f89c9770c8f59ccb11633717addb6
GruescuDanziel <gruescu.ipatie@gmail.com>
2020-12-28 11:47:32 +0200

    Forgot to add the remove function at choices

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from WebsiteClass import Website
 import questions as qs
 
 initialAction = int(input("What do you wish to do: 1. Make a new webSite. 2. Edit an old save"))
-posibleChoices = ['getData', 'createFolder', 'goBackAFolder','createFile', 'checkEntireStructure', 'checkCurrentFolder', 'editFile','checkFile', 'changeFolder', 'endEditing']
+posibleChoices = ['getData', 'createFolder', 'goBackAFolder','createFile', 'checkEntireStructure', 'checkCurrentFolder', 'editFile','checkFile', 'removeCode', 'changeFolder', 'endEditing']
 
 
 if initialAction == 1:
